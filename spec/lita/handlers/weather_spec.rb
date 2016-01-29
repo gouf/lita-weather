b7bab0a0e2fc396ccf1afe2335223943e1828979
Gou Furuya <innocent.zero@gmail.com>
2016-01-29 19:36:37 +0900

    Add rough spec

--- spec/lita/handlers/weather_spec.rb
+++ spec/lita/handlers/weather_spec.rb
@@ -1,4 +1,15 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe Lita::Handlers::Weather, lita_handler: true do
+  it { is_expected.to route('weather tokyo') }
+  it { is_expected.to route('weather tokyo').to(:weather) }
+  it 'retrieved message includes "Area: Tokyo"' do
+    send_command('weather tokyo')
+    expect(replies).to include('Area: Tokyo')
+  end
+
+  it 'JSON response includes a key as text' do
+    response = http.post '/weather', text: 'yokohama'
+    expect(JSON.parse(response.body).keys).to include('text')
+  end
 end
